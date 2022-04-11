d58716c628efb5490ff1f2a52b5e713b72646155
JL <lambertjefferson66@gmail.com>
2022-04-11 15:25:19 -0600

    .

--- index.js
+++ index.js
@@ -23,8 +23,31 @@ let knex = require("knex")({
 app.use(express.urlencoded({extended: true}));
 
 app.listen(listenPort, function() {console.log("Listener active on port " + listenPort);});
-
-
+// HEROKU STUFF
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+app
+    .get("/", (req,res) => {
+        res.render("addvehicle");
+    })
+    .get('/db', async (req, res) => {
+        try {
+          const client = await pool.connect();
+          const result = await client.query('SELECT * FROM test_table');
+          const results = { 'results': (result) ? result.rows : null};
+          res.render('pages/db', results );
+          client.release();
+        } catch (err) {
+          console.error(err);
+          res.send("Error " + err);
+        }
+      })
+      
 app.get("/deleteItem/:vehicle_id",(req,res) => {
     knex("Vehicle").where('vehicle_id', req.params.vehicle_id).del()
 .then (() =>{
